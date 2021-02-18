068d85e1e9a47111e71aa7c95d74cb9ef6c13ed6
AlexandreFsilva <alex-afs@hotmail.com>
2021-02-17 22:16:36 -0300

    Entendendo os comandos nativos JavaScript e laços de repetição

---
+++ JavaScript/main.js
@@ -0,0 +1,44 @@
+    /* Testando variáveis e alert*/
+    
+    /*var caixa= prompt("Qual valor de caixa");
+    var nome="Alexandre";
+    var curso= "JavaScript";
+    var sobrenome="Ferreira da Fonseca";
+    var lista=["Pequeno ", "Médio" , "Grande"];
+    var fruta={nome:"maça", cor:"Vermelha"}/* Dicionário*/
+    /*var frutas=[{nome:"maça",cor:"rosa"} , {nome:"laranja",cor:"verde"}]
+    lista.push("ExtraGrande");/*Insere um item na lista*/
+    /*lista.pop();/* Retira último item da lista*/
+  /*  alert("Bem vindo " + nome.toUpperCase());/* Cria alerta 
+
+    /* Utilizando o console do navegador
+    console.log(nome)*/
+    /* Utilizando replace nos comandos 
+    console.log("Meu nome é "+ nome + sobrenome.replace("Ferreira da Fonseca"," Silva"));*/
+   /*console.log("Meu nome é " + nome + "e estou cursando " + curso.toLowerCase() );*/
+    /* Utilizando listas*/
+   /* console.log(lista)
+    console.log(lista[2]);
+    console.log(lista[1]);
+    console.log(lista.length);/* Para verificar lista*/
+   /* console.log(lista.reverse());/* Traz os elementos ao contrário na lista*/
+/*console.log(lista.toString());/* Traz a string da lista*/
+/*console.log(lista.join(" - " ));/* Traz a string da lista com a definição da separação*/
+/*console.log(fruta.nome + fruta.cor);/* concatenação do dicionário*/
+/*console.log(frutas[1].nome);/* busca a posição 1 da lista de dicionários*/
+/*if(caixa>1) /* utilização condicional*/
+/*{alert("O caixa está positivo");}
+else{ 
+    /*alert("o caixa está negativo");*/
+
+/*while(contador<=4){/*estrutura de repetição*/
+    /*console.log(contador);
+    contador++
+}*/
+/*var contador;
+for (contador=1; contador<=10 ; contador ++) {console.log(contador)} o for funciona como durante*/
+var d= new Date();
+alert(d.getDay());
+
+
+  
